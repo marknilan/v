bd667e9d8b64b304232af59d0dba7f1b4171afca
mark7498 <mark7498@outlook.com>
2025-06-10 07:16:38 +0800

    Tidy up modelling code

--- mpgsrc/mpg.v
+++ mpgsrc/mpg.v
@@ -1,9 +1,9 @@
 module mpgsrc
 
 // mpg mpgsrc mpg.v
-import startup
-import injest
-import poemcode
+import startup // all things parm and config
+import injest // all things incoming data
+import poemcode // all things poem generation
 
 // runmpg runs the mpg program
 pub fn runmpg() !bool {
@@ -21,21 +21,24 @@ pub fn runmpg() !bool {
 	meter_templates := injest.getmtrtempl(mpgpaths.wrdpath + 'meter_templates.csv')
 	// splitting word data by type	
 	nouns, verbs, adjectives, pronouns, determiners, interjections, conjunctions, prepositions, adverbs := injest.makelists(mpgwords)
-	// create virtual DB (lists) schema
+	// create virtual DB (as lists in memory) in essence a pseudo DB schema
 	mut listdbs := injest.listdbs(mpgwords, nouns, verbs, adjectives, pronouns, determiners,
 		interjections, conjunctions, prepositions, adverbs)
-	// store in List DB rhyming rules
+	// store in List DB the rhyming rules
 	listdbs.rhyme_roots = injest.getrhymeroots(mpgpaths.wrdpath + 'rhyme_roots.csv')
-	// virtual Db schema metadata
+	// strore the virtual List DB schema metadata
 	listdbs.mpgcounts = injest.make_list_counts(listdbs)
 	println('\nMPG word metadata is as follows \n: ${listdbs.mpgcounts}')
+	// trains the models
 	if runmode.to_lower() in ['-t', 't'] {
 		injest.rwpoemtemplate(mpgpaths.wrdpath + '/notused/rondeautraining1.txt', mpgwords, poem)
 		println('New templates written to ${mpgpaths.tmpdir} mpgtemplates.txt')
 	}
+	// displays models
 	if runmode.to_lower() in ['-m', 'm'] {
 		poemcode.run_model(poem, runmode, meter_templates, listdbs, mpgpaths.tmpdir)
 	}
+	// generates poems from models
 	if runmode.to_lower() in ['-g', 'g'] {
 		poemcode.run_generate(poem, runmode, meter_templates, listdbs, mpgpaths.tmpdir)
 	}
